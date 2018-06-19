0cb59614b5de602352e06a2963c822591291b5ef
lsohr <40297755+lsohr@users.noreply.github.com>
2018-06-19 11:17:27 +0200

    guii

--- Cocktailbar/src/gui/FirstWindow.java
+++ Cocktailbar/src/gui/FirstWindow.java
@@ -1,5 +1,14 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class FirstWindow {
+
+    @FXML
+    private Button ButtonBestellung;
+
+    @FXML
+    private Button ButtonNewCocktail;
 
 }
