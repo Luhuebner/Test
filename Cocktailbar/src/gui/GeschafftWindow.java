eae31e0bbba245b009d52dc679a95a7a5d5ad83a
lsohr <40297755+lsohr@users.noreply.github.com>
2018-06-19 11:16:27 +0200

    Gui

---
+++ Cocktailbar/src/gui/GeschafftWindow.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GeschafftWindow {
+
+    @FXML
+    private Label lblGlueckwunsch;
+
+    @FXML
+    private Label lblErstellt;
+
+}
