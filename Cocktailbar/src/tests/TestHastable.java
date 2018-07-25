82b8775066e186d8fcbc4de86f129da9c1ba432c
Luhuebner <30832867+Luhuebner@users.noreply.github.com>
2018-07-25 20:26:48 +0200

    fabain fixed it

--- Cocktailbar/src/tests/TestHastable.java
+++ Cocktailbar/src/tests/TestHastable.java
@@ -12,33 +12,45 @@ import arten.Saft;
 public class TestHastable {
 
 	public static void main(String[] args) {
-		Hashtable<String, Einzelgetraenk> h = new Hashtable();
+		Hashtable<String, Einzelgetraenk> h = null;
+		// = new Hashtable();
 		try {
 			FileInputStream fs = new FileInputStream("Zutaten.cocktail");
 			ObjectInputStream in = new ObjectInputStream(fs);
-			for (int i = 0; i < h.size(); i++) {
-				h =  in.readObject();
-			}
+			// for (int i = 0; i < h.size(); i++) { bin heir nie reingegangen lol
+			h = (Hashtable<String, Einzelgetraenk>) in.readObject();
+			// }
 
 			in.close();
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
-		
-		
-		
-		
+
+		if (h == null)
+			h = new Hashtable<String, Einzelgetraenk>();
+
+		if (h.isEmpty()) {
+			System.out.println("fuck you leatherman");
+			loadme(h);
+		}
+		// h.put("Birnensaft", new Saft("Birnensaft", 47, 2.0));
+		System.out.println(h.get("Birnensaft").getKalorien());
+
 		try {
+
 			FileOutputStream fs = new FileOutputStream("Zutaten.cocktail");
 			ObjectOutputStream out = new ObjectOutputStream(fs);
 			for (int i = 0; i < h.size(); i++) {
 				out.writeObject(h);
 			}
-
 			out.close();
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
+	}
+
+	public static void loadme(Hashtable<String, Einzelgetraenk> h) {
+		h.put("Birnensaft", new Saft("Birnensaft", 47, 2.0));
 	}
 
 }
