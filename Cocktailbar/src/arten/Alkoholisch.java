189203530ad7ba645ae0ad762e8e80be761a2e2a
Luhuebner <30832867+Luhuebner@users.noreply.github.com>
2018-08-10 08:30:21 +0200

    comments

--- Cocktailbar/src/arten/Alkoholisch.java
+++ Cocktailbar/src/arten/Alkoholisch.java
@@ -1,7 +1,7 @@
 package arten;
 
 public class Alkoholisch extends Einzelgetraenk {
-
+// Konstruktor Alkoholisch
 	public Alkoholisch(String name, double kcal, double alk, double preis) {
 		super(name, kcal, alk, preis);
 
