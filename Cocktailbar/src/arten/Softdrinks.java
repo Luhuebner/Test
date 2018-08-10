34550580b600b54b131a9de04feb9ac1f8fc4e32
Luhuebner <30832867+Luhuebner@users.noreply.github.com>
2018-08-10 08:32:55 +0200

    comments

--- Cocktailbar/src/arten/Softdrinks.java
+++ Cocktailbar/src/arten/Softdrinks.java
@@ -2,7 +2,7 @@ package arten;
 
 public class Softdrinks extends Alkoholfrei{
 
-	
+	// Konstruktor Softdrinks
 	public Softdrinks(String Name, double kcal, double preis) {
 		super(Name, kcal, preis);
 	}
