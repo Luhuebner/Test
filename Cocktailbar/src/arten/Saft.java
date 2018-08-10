2446a8af545cec499b6662ff6f675fd26df54182
Luhuebner <30832867+Luhuebner@users.noreply.github.com>
2018-08-10 08:32:33 +0200

    coments

--- Cocktailbar/src/arten/Saft.java
+++ Cocktailbar/src/arten/Saft.java
@@ -3,7 +3,7 @@ package arten;
 import java.io.Serializable;
 
 public class Saft extends Alkoholfrei implements Serializable{
-
+	// Konstruktor Saft
 	public Saft(String name, double kcal, double preis) {
 		super(name, kcal,preis);
 		// TODO Auto-generated constructor stub
